db418ae987c08bf2c1b0c3a3a591e212b365caa5
Marta Kocmidova <mochomurka6@seznam.cz>
2020-09-27 16:13:48 +0200

    created react app, added palette

---
+++ src/Pallete.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Pallete extends Component {
+  render() {
+    return (
+      <div className='palette'>
+        <div className='palette-colors'></div>
+      </div>
+    )
+  }
+}
