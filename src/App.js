d5faf3ee54921dd6f76a6e8134d1e395f95d8a4d
Marta Kocmidova <mochomurka6@seznam.cz>
2020-09-28 20:23:24 +0200

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom'
 import Pallete from './components/Pallete'
 import seedColors from './seedColors'
 import {generatePalette} from './colorHelpers'
+import { Palette } from '@material-ui/icons';
 
 function App() {
   console.log(generatePalette(seedColors[4]))
+
+  const findPalette = (id) => {
+     return seedColors.find(palette => palette.id === id )
+  }
   return (
-    <div>
-     <Pallete palette={generatePalette(seedColors[4])}/>
-    </div>
+    <Switch>
+      <Route exact path='/' render={() => <h1>Ahoj</h1>} />
+      <Route exact path='/palette/:id' render={(routeProps) => <Pallete 
+      palette={
+        generatePalette(findPalette(routeProps.match.params.id))
+      } 
+      />} />
+    </Switch>
+    // <div>
+    //  <Pallete palette={generatePalette(seedColors[4])}/>
+    // </div>
   );
 }
 
