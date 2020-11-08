55cf7d9e83c48e0eb135027cc63efabded323aea
Marta Kocmidova <mochomurka6@seznam.cz>
2020-11-08 20:33:49 +0100

    Add classnames to colorBox

--- src/components/color-box.js
+++ src/components/color-box.js
@@ -1,55 +1,72 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
-import { withStyles } from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles'
 import styles from '../styles/color-box-styles'
+import classNames from 'classnames'
 import {CopyToClipboard} from 'react-copy-to-clipboard'
 
 class ColorBox extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { copied: false }
-    this.changeCopyState = this.changeCopyState.bind(this)
-  }
+	constructor(props) {
+		super(props)
+		this.state = {copied: false}
+		this.changeCopyState = this.changeCopyState.bind(this)
+	}
 
-  changeCopyState() {
-    this.setState({copied: true}, () => {
-      setTimeout(() => this.setState({copied: false}), 1500)
-    })
-  }
+	changeCopyState() {
+		this.setState({copied: true}, () => {
+			setTimeout(() => this.setState({copied: false}), 1500)
+		})
+	}
 
-  render() {
-    const {name, background, paletteId, id, showingFullPalette, classes} = this.props
-    const {copied} = this.state
+	render() {
+		const {
+			name,
+			background,
+			paletteId,
+			id,
+			showingFullPalette,
+			classes,
+		} = this.props
+		const {copied} = this.state
 
-
-    return (
-      <CopyToClipboard  text={background} onCopy={this.changeCopyState}>
-        <div style={{background}} className={classes.colorBox}>
-          <div style={{background}} 
-               className={`${classes.copyOverlay} ${copied && classes.showOverlay}`} 
-          />
-          <div className={`${classes.copyMsg} ${copied && classes.showCopyMsg}`}>
-            <h2>Copied!</h2>
-            <p className={classes.copyText}>{background}</p>
-          </div>
-          <div>
-            <div className={classes.boxContent}>
-              <span className={classes.colorName}>{name}</span>
-            </div>
-            <button className={classes.copyButton}>Copy</button>
-          </div>
-          {/* stop propagation zajišťuje aby se nespustila jiná akce 
+		return (
+			<CopyToClipboard text={background} onCopy={this.changeCopyState}>
+				<div style={{background}} className={classes.colorBox}>
+					<div
+						style={{background}}
+						className={classNames(classes.copyOverlay, {
+							[classes.showOverlay]: copied,
+						})}
+					/>
+					<div
+						className={classNames(classes.copyMsg, {
+							[classes.showCopyMsg]: copied,
+						})}
+					>
+						<h2>Copied!</h2>
+						<p className={classes.copyText}>{background}</p>
+					</div>
+					<div>
+						<div className={classes.boxContent}>
+							<span className={classes.colorName}>{name}</span>
+						</div>
+						<button className={classes.copyButton}>Copy</button>
+					</div>
+					{/* stop propagation zajišťuje aby se nespustila jiná akce
           než ten link, aby to i nezkopírovalo barvu jako zbytek color boxu */}
-          {/* check relative routes in react router - přístup k informaci kde zrovna jsme / current url */}
-          {showingFullPalette && (
-            <Link to={`/palette/${paletteId}/${id}`} onClick={e => e.stopPropagation}>
-              <span className={classes.seeMore}>More</span>
-            </Link>
-          )}
-        </div>
-      </CopyToClipboard>
-    )
-  }
+					{/* check relative routes in react router - přístup k informaci kde zrovna jsme / current url */}
+					{showingFullPalette && (
+						<Link
+							to={`/palette/${paletteId}/${id}`}
+							onClick={(e) => e.stopPropagation}
+						>
+							<span className={classes.seeMore}>More</span>
+						</Link>
+					)}
+				</div>
+			</CopyToClipboard>
+		)
+	}
 }
 
 export default withStyles(styles)(ColorBox)
--- src/components/mini-palette.js
+++ src/components/mini-palette.js
@@ -1,35 +1,48 @@
 import React from 'react'
-import { withStyles } from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles'
 import styles from '../styles/mini-palette-styles'
-import DeleteIcon from "@material-ui/icons/Delete";
+import DeleteIcon from '@material-ui/icons/Delete'
 
-function MiniPalette (props) {
-  const {classes, paletteName, emoji, colors, openDialog, id} = props
-  const miniPaletteBoxes = colors.map(color => (
-    <div
-      className={classes.miniColor}
-      style={{backgroundColor: color.color}}
-      key={color.name}
-    />
-  ))
+function MiniPalette(props) {
+	const {
+		classes,
+		paletteName,
+		emoji,
+		colors,
+		openDialog,
+		id,
+		goToPalette,
+	} = props
+	const miniPaletteBoxes = colors.map((color) => (
+		<div
+			className={classes.miniColor}
+			style={{backgroundColor: color.color}}
+			key={color.name}
+		/>
+	))
 
-  const deletePalette = (e) => {
-    e.stopPropagation()
-    openDialog(id)
-  }
+	const handleClick = () => {
+		goToPalette(props.id)
+	}
 
-  return (
-    <div className={classes.root} onClick={props.handleClick}>
-        <DeleteIcon className={classes.deleteIcon} onClick={deletePalette} />
-      <div className={classes.colors}>
-        {miniPaletteBoxes}
-      </div>
-      <h5 className={classes.title}>
-        {paletteName}
-        <span className={classes.emoji}>{emoji}</span>
-      </h5>
-    </div>
-  )
+	const deletePalette = (e) => {
+		e.stopPropagation()
+		openDialog(id)
+	}
+
+	return (
+		<div className={classes.root} onClick={handleClick}>
+			<DeleteIcon
+				className={classes.deleteIcon}
+				onClick={deletePalette}
+			/>
+			<div className={classes.colors}>{miniPaletteBoxes}</div>
+			<h5 className={classes.title}>
+				{paletteName}
+				<span className={classes.emoji}>{emoji}</span>
+			</h5>
+		</div>
+	)
 }
 
 export default withStyles(styles)(MiniPalette)
