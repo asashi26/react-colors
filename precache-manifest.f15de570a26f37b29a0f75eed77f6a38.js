97b5e3e6bbf660f8c8785f6d682115182e4a6372
Marta Kocmidova <mochomurka6@seznam.cz>
2020-11-13 21:13:20 +0100

    Updates

---
+++ precache-manifest.f15de570a26f37b29a0f75eed77f6a38.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e054e06008dad7df9de809ed425d427e",
+    "url": "/react-colors/index.html"
+  },
+  {
+    "revision": "8ecf93bdf43867b47c2d",
+    "url": "/react-colors/static/css/2.0da342d8.chunk.css"
+  },
+  {
+    "revision": "38f84bd89c97f695b42c",
+    "url": "/react-colors/static/css/main.4078b5b7.chunk.css"
+  },
+  {
+    "revision": "8ecf93bdf43867b47c2d",
+    "url": "/react-colors/static/js/2.2898b025.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/react-colors/static/js/2.2898b025.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38f84bd89c97f695b42c",
+    "url": "/react-colors/static/js/main.e6a999d7.chunk.js"
+  },
+  {
+    "revision": "bb07e0b96fc8d3ac97e1",
+    "url": "/react-colors/static/js/runtime-main.c895aa97.js"
+  },
+  {
+    "revision": "b2a1b623bec6516fbe684fc6a93b6962",
+    "url": "/react-colors/static/media/bg.b2a1b623.svg"
+  }
+]);
